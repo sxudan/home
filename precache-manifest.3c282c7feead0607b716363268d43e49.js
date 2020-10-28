38ba63e99ad26592b4f02882522f384c160b5cf1
Sudan Suwal <sudan.suwal@spiralogics.com>
2020-10-28 16:52:47 +0545

    Updates

---
+++ precache-manifest.3c282c7feead0607b716363268d43e49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4db9b4a75802876745f97af16880204",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0168dcd9e0cf5262508f",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "beee62f9b07d13c36645",
+    "url": "/home/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "0168dcd9e0cf5262508f",
+    "url": "/home/static/js/2.3f771261.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.3f771261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beee62f9b07d13c36645",
+    "url": "/home/static/js/main.188da256.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "4467407a89ef6b5dde2c01457ebffd9d",
+    "url": "/home/static/media/resume.4467407a.pdf"
+  }
+]);
